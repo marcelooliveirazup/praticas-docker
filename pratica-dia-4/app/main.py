10446fdd2ee509d01dab8340d4e50a06a51dc54e
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-05-20 11:08:16 -0300

    Muda para rodar com localstack

--- pratica-dia-4/app/main.py
+++ pratica-dia-4/app/main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+import boto3
+import os
 
-app = Flask(__name__)
+endpoint_url = os.environ.get("AWS_ENDPOINT_URL", "http://localhost:4566")
 
-@app.route('/')
-def home():
-    return "Olá, mundo! App Python com CI/CD e LocalStack!"
+s3 = boto3.client(
+    "s3",
+    region_name="us-east-1",
+    aws_access_key_id="test",
+    aws_secret_access_key="test",
+    endpoint_url=endpoint_url
+)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+bucket_name = "meu-bucket-local"
+
+s3.create_bucket(Bucket=bucket_name)
+print(f"Bucket '{bucket_name}' criado com sucesso.")
+
+response = s3.list_buckets()
+for bucket in response["Buckets"]:
+    print(f" - {bucket['Name']}")
